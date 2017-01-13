7620eb67e2aa22b6d39dc5a947bf9eefcae32646
sharecover <mycovergame@gmail.com>
2017-01-13 21:48:06 +0700

    Add files via upload

--- safelink3OL3.1.js
+++ safelink3OL3.1.js
@@ -19,13 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
-var xxx=null;
-if(x=="1"){xxx="visit-link-1.html"}
-if(x=="2"){xxx="visit-link-2.html"}
-if(x=="3"){xxx="visit-link-3.html"}
-if(x=="4"){xxx="visit-link-4.html"}
-if(x=="5"){xxx="visit-link-5.html"}a_to_ve[i].href="http://www.openlink.us/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+a_to_ve[i].href="http://www.openlink.us/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
